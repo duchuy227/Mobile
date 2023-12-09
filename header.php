4c3d19cad431f09aabea5c1eaab8f5b8d1f82d2d
Duc Huy <103496061+duchuy2278@users.noreply.github.com>
2023-12-09 17:33:29 +0700

    hh

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <section class="main">
     <nav class="navbar navbar-expand-lg" style="background-color:blanchedalmond;">
-        <a class="navbar-brand" href="#"><img src="Image/logo.jpeg" width="70" height="70" alt="" ></a>
+        <a class="navbar-brand" href="#">ARISTILO</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
--- headeradmin.php
+++ headeradmin.php
@@ -13,7 +13,7 @@
 </style>
 
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color:blanchedalmond;">
-<a class="navbar-brand"><img src="Image/logo.jpeg" width="70" height="70" alt="" ></a>
+<a class="navbar-brand">ARISTILO</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
